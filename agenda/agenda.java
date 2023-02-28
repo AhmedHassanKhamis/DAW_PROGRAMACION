2d6615fbd55038c806d94c2e1c7a0d157e98b0cd
Ahmed Hassan Khamis <92717109+AhmedHassanKhamis@users.noreply.github.com>
2023-02-28 18:11:27 +0100

    Add files via upload

---
+++ agenda/agenda.java
@@ -0,0 +1,24 @@
+package agenda;
+
+public class agenda {
+	 private contacto[] array1;
+	 private int numero;
+
+	 
+	 
+	public agenda() {
+		numero = 0;
+		array1 = new contacto[10];
+	}
+	
+	public agenda(int tamanio) {
+		numero = 0;
+		array1 = new contacto[tamanio];
+	}
+	 
+	 
+	 
+	
+	
+	
+}
---
+++ agenda/contacto.java
@@ -0,0 +1,23 @@
+package agenda;
+
+public class contacto {
+	private String nombre;
+	private int numero;
+	
+	
+	public contacto(String nombre, int numero) {
+		this.nombre = nombre;
+		this.numero = numero;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public int getNumero() {
+		return numero;
+	}
+	
+}
